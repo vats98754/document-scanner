a451beb089886debd2970ec62c7d552c804c30c1
vats98754 <a7vats@uwaterloo.ca>
2025-07-20 03:52:54 +0530

    added efficient approach and mygetGaussianKernel helper function to implement 1D Gaussian kernel from scratch

--- FromScratchGaussianBlur.py
+++ FromScratchGaussianBlur.py
@@ -12,23 +12,38 @@ class FromScratchGaussianBlur():
 def gaussian(x, sigmaX=1, mu=0):
     return (1 / (sigmaX * (2*math.pi)**0.5) * math.e ** (-0.5 * ((x-mu)/sigmaX)**2))
 
-def myGaussianKernel(ksize=(3,3), sigmaX=1.0):
-    # get the center of the kernel
-    center_y = (ksize[0]-1)/2
-    center_x = (ksize[1]-1)/2
-    sum = 0 # for normalization
-    kernel = []
-    for y in range(ksize[0]):
-        row = []
-        for x in range(ksize[1]):
-            delta_y = y - center_y
-            delta_x = x - center_x
-            gaussian_x_y = gaussian((delta_x**2 + delta_y**2)**0.5, sigmaX)
-            sum += gaussian_x_y
-            row.append(gaussian_x_y)
-        kernel.append(row)
-    kernel_np = np.array(kernel, dtype=float)
-    return kernel_np/sum
+def mygetGaussianKernel(dim=1, sigmaX=1.0):
+    sum_x = 0 # for normalization
+    center_x = (dim-1)/2 # get the center of the 1D kernel
+    op = []
+    for x in range(dim):
+        delta_x = abs(x-center_x)
+        gaussian_x = gaussian(delta_x, sigmaX)
+        sum_x += gaussian_x
+        op.append(gaussian_x)
+    return np.array(op, dtype=float)/sum_x
+
+def myGaussianKernel(ksize=(3,3), sigmaX=1.0):    
+    # OPTIMIZED approach, using outer product of computer 1D Gaussian kernels in x and y directions
+    return np.outer(mygetGaussianKernel(ksize[0], sigmaX),
+                    mygetGaussianKernel(ksize[1], sigmaX))
+
+    # NAIVE approach, not optimized
+    # kernel = []
+    # # get the center of the 2D kernel
+    # center_y = (ksize[0]-1)/2; center_x = (ksize[1]-1)/2
+    # sum = 0 # for normalization
+    # for y in range(ksize[0]):
+    #     row = []
+    #     for x in range(ksize[1]):
+    #         delta_y = y - center_y
+    #         delta_x = x - center_x
+    #         gaussian_x_y = gaussian((delta_x**2 + delta_y**2)**0.5, sigmaX)
+    #         sum += gaussian_x_y
+    #         row.append(gaussian_x_y)
+    #     kernel.append(row)
+    # kernel_np = np.array(kernel, dtype=float)
+    # return kernel_np/sum
     
 if __name__ == "__main__":
     kernel_height = 5
